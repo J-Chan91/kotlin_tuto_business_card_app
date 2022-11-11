2818e6d255999c48578146220d26a5c3180221b8
Chan <95342579+J-Chan91@users.noreply.github.com>
2022-11-11 23:53:24 +0900

    complete

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +35,8 @@ class MainActivity : ComponentActivity() {
             BusinessCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android!")
+                    InitContainer()
                 }
             }
         }
@@ -30,14 +44,111 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BackgroundContainer() {
+    FirstContainer()
+    SecondContainer()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun FirstContainer() {
+    val boxModifier = Modifier.fillMaxWidth()
+
+
+    Column(
+        modifier = boxModifier, horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        FirstContainerLogoComposable()
+        FirstContainerMyNameComposable()
+        FirstContainerJobComposable()
+    }
+}
+
+@Composable
+fun SecondContainer() {
+    val secondContainerModifier =
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp)
+
+    Column(modifier = secondContainerModifier) {
+        SecondContainerRowContainer(Icons.Rounded.Call, "010-1234-5678")
+        SecondContainerRowContainer(Icons.Rounded.Share, "JongChan")
+        SecondContainerRowContainer(Icons.Rounded.Email, "dev.jchan91@gmail.com")
+    }
+}
+
+@Composable
+fun FirstContainerLogoComposable() {
+    val img = painterResource(R.drawable.android_logo)
+    val imgModifier = Modifier.size(100.dp)
+
+    Image(painter = img, modifier = imgModifier, contentDescription = "logo")
+}
+
+@Composable
+fun FirstContainerMyNameComposable() {
+    Text(
+        text = "JongChan Park",
+        color = Color.White,
+        fontSize = 38.sp,
+        fontWeight = FontWeight.Thin
+    )
+}
+
+@Composable
+fun FirstContainerJobComposable() {
+    Text(
+        text = "Frontend Developer",
+        color = colorResource(id = R.color.android_main_color),
+        fontSize = 15.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun SecondContainerRowContainer(target: ImageVector, contents: String) {
+    val rowModifier =
+        Modifier
+            .padding(vertical = 5.dp)
+//            .fillMaxWidth()
+//            .border(width = 0.3.dp, Color.White)
+
+    Row(modifier = rowModifier) {
+        IconComposable(target)
+        ContentsComposable(contents)
+    }
+}
+
+@Composable
+fun IconComposable(target: ImageVector) {
+    val iconModifier = Modifier.size(25.dp)
+
+    Icon(
+        target,
+        contentDescription = "phone",
+        modifier = iconModifier,
+        tint = colorResource(id = R.color.android_main_color)
+    )
+}
+
+@Composable
+fun ContentsComposable(contents: String) {
+    val contentsModifier = Modifier.padding(horizontal = 20.dp)
+
+    Text(text = contents, modifier = contentsModifier, color = Color.White)
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun InitContainer() {
+    val initContainerModifier = Modifier.background(colorResource(id = R.color.main_background))
+
     BusinessCardTheme {
-        Greeting("Android")
+        Box(modifier = initContainerModifier, contentAlignment = Alignment.Center) {
+            FirstContainer()
+        }
+        Box(contentAlignment = Alignment.BottomCenter) {
+            SecondContainer()
+        }
     }
 }
